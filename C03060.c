f9342d59522c9551ad15f63e939272fa4d92d9c3
TYP_LongHD <154016333+duylonggg@users.noreply.github.com>
2024-04-18 00:04:56 +0700

    C03060.c

--- C03060.c
+++ C03060.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
 #include <math.h>
-int check(int n, int k)
-{
+int check(int n, int k) {
 	int cnt = 0;
-	for(int i = 1; i <= n; ++i)
-	{
+	for (int i = 1; i <= n; ++i) {
 		int check = i;
-		while(check % 2 == 0)
-		{
+		while (check % 2 == 0) {
 			++cnt;
 			check /= 2;
 		}
-		if(cnt >= k)
-			return 1;
+		if (cnt >= k) return 1;
 	}
 	return 0;
 }
-int main()
-{
+int main() {
 	int n, k;
 	scanf("%d%d", &n, &k);
-	if(check(n, k))
-		printf("Yes");
-	else
-		printf("No");
+	printf("%s", check(n, k) ? "YES" : "NO");
     return 0;
 }
